d9e0e208ab7479b7656c2372f4592ce51349e644
ilamiheev <instantofeternity@gmail.com>
2019-09-12 11:35:28 +0300

    2.0 ver

--- make_input.py
+++ make_input.py
@@ -5,10 +5,22 @@ import numpy as np
 from fMRI_Regions import get_masked_fmri
 
 
+# make new atribute with labels of fmri. Call new atribute in methods
+# make something with marks (now we need two files - set and edf. Problem with eeglab that converts set to edf
+# and change marks(events) with letters to numbers - it affects generalisation of our program. I need to extract events
+# file from eeglab and extract events
+# make filtration from 5 to 100 (i want to find delay in 5 seconds)
+# Why positive coefficients (positive correaltion) in undershoot peak?
+# Draw on one picture coefficients, patterns and predict time series
+# Make with pandas two tables with results with helium pump on and off
+# Draw left and right amigdalla and hypotalamus
+# Add number of dimensions of input
+# Create new dataset for CCA (1-channel and voxels in cortex)
+
+
 class Dataset:
     def __init__(self, random_seed=42, segment_length=16384, eeg_padded=False,
                  frame_creation_time=1950, step=195):
-
         self.eeg_padded = eeg_padded
         self.segment_length = segment_length
         self.frame_creation_time = frame_creation_time
@@ -19,12 +31,12 @@ class Dataset:
         vector_exclude = ['EOG', 'ECG', 'CW1', 'CW2', 'CW3', 'CW4', 'CW5', 'CW6', 'Status']
         raw = mne.io.read_raw_edf(eeg_path, exclude=vector_exclude)
         eeg = raw.get_data()
-        eeg = mne.filter.filter_data(eeg, sfreq=1000, l_freq=5, h_freq=60)
+        eeg = mne.filter.filter_data(eeg, sfreq=1000, l_freq=5, h_freq=100)
         eeg_flip = np.fliplr(eeg)
         fmri_im = image.smooth_img(fmri_path, fwhm=6)
         fmri = get_masked_fmri(fmri_im, "sub")
         start = start_time - self.segment_length
-        end = start_time 
+        end = start_time
         x_list = []
         y_list = []
         x_fl_list = []
--- methods.py
+++ methods.py
@@ -23,8 +23,38 @@ def make_a(regres, x_train):
         a_list.append(a)
     return np.array(a_list)
 
+# make new attribute with covariance array and plot it
+# i need to add temporal_cca to methods
+# add noise statistics
+# fmri activity between fmri regions
+def patterns_evaluation(a, len_zones, x_train, x_test):
+    real_coefs: List[None] = []
+    n= []
+    for s in range(len_zones - 1):
+        coefs = np.array(a)
+        mean_coefs = coefs
+        #mean_coefs = mean_coefs.reshape(np.shape(x_test)[2], np.shape(x_test)[3])
+        vect = []
+        for i in range(np.shape(x_train)[0]):
+            S1 = x_train[i, best_param[s][0], ...].reshape(-1)
+            vect.append(S1)
+        vect = (np.array(vect)).T
+        vect2 = []
+        for tao in range(np.shape(x_train)[0]):
+            S2 = np.dot(mean_coefs.reshape(-1, 1).T, vect[:, tao])
+            vect2.append(S2)
+        vect2 = (np.array(vect2)).T
+        Final = []
+        for tim in range(np.shape(vect)[0]):
+            Final.append(np.cov(vect[tim, :], vect2)[0, 1])
+        Final = np.array(Final)
+        Final = Final.reshape(np.shape(x_test)[2], np.shape(x_test)[3])
+        real_coefs.append(Final)
+    N =
+    return real_coefs, N
 
-# def patterns_evaluation(x_):
+
+
 # return None
 
 
@@ -36,7 +66,7 @@ def make_b(regres, x_train):
     return np.array(b_list)
 
 
-# сделать через насследование c super_init_
+# make with inheritance (use super_init_ method)
 
 def simple_linear_regression(alphas, repr_train, repr_val, y_train, y_val, zone, normalize):
     scores_mse = []
@@ -51,12 +81,15 @@ def simple_linear_regression(alphas, repr_train, repr_val, y_train, y_val, zone,
     coefs = models_a1[ind].coef_
     model = models_a1[ind]
     return np.array(coefs), model
-
-
+#шум как регул
+#статистики шумов
+# make noize with latent variiables a noize
 # x_train dimensions N*FT
-
+#optimisie with results from ret
 
 class linear_pattern_recognition:
+#class for finding patterns of ROI
+
 
     def __init__(self, alphas=np.logspace(-3, 3, 7), normalize=False, epsilon=0.5):
         self.alphas = alphas
@@ -69,13 +102,15 @@ class linear_pattern_recognition:
         self.mse_scores = None
         self.pearson_scores = None
         self.r2_scores = None
+        self.patterns_topo_ = None
+        self.patterns_spect_ = None
 
     def fit(self, x_train, x_val, y_train, y_val):
         self.len_zones = np.shape(y_train)[1]
         len_chan = np.shape(x_train)[1]
         len_spect = np.shape(x_train)[2]
         models = []
-        topo_coefs = []
+        topo_coefs: List[None] = []
         spect_coefs = []
         for zone in range(self.len_zones):
             topo_regr0 = np.random.rand(1, len_chan)
@@ -109,6 +144,8 @@ class linear_pattern_recognition:
         self.topo_coefs_ = topo_coefs
         self.spect_coefs_ = spect_coefs
         self.model = models
+        self.patterns_spect_ = patterns_evaluation(topo_coefs)
+        self.patterns_topo_ = patterns_evaluation(spect_coefs)
         return self
 
     def test(self, x_test, y_test):
@@ -116,6 +153,7 @@ class linear_pattern_recognition:
 
         :rtype: object
         """
+
         predicted_values = []
         scores_mse = []
         scores_r2 = []
